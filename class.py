600f28bebd47bd3e7e0e242ad8ebb98acdf8dbae
diego rivera <diegora1377@gmail.com>
2020-01-14 23:08:47 -0600

    class.py

---
+++ class.py
@@ -0,0 +1,131 @@
+
+# print("""I'm a string
+# sleejnd
+# sljlekjs
+# skejfj
+# fjeijlije
+#         """)
+
+# print("I'm a string")
+# print("skdfjlkefvngil")
+
+#concatenating
+
+# print("hello " + "world")
+
+# print("""I\'m a\t\t\t\tstring
+# asdlklendlkg
+# \v\v\v\v
+# """)
+
+# a = 20
+# b = 15
+# c = 3
+
+# print(a + b + c)
+
+# name = "Diego"
+# lname = "Rivera"
+
+# output = "good morning {0} {1} {1} {0}".format(name, lname)
+
+# print(output)
+
+# student1 = "john"
+# student2 = "diego"
+# output = f'hello world {student1} {student2}'
+
+# print(output)
+
+# print(type(student1))
+
+# test = '5'
+# test1 = '5' + 5
+# print(isinstance(test1, int))
+
+# consolelog = input("Enter your name >>")
+# output = f'Hello {consolelog}'
+# dataType = type(consolelog)
+# print(dataType)
+
+# input_number_from_user = input("Insert a number >>")
+
+# print(type(input_number_from_user))
+
+# casted_output = int(input_number_from_user)
+
+# print(type(casted_output))
+
+# some_math = casted_output *7
+# print(some_math)
+
+
+# age = 26
+# if True :
+#     print(age)
+
+# print(age)
+
+
+# name = input("Enter in your name >>")
+# if (name == "Diego"):
+#     print(name)
+
+# name = "Diego"
+# age = 26
+
+# if (age <= 25):
+#     print(age)
+
+# if (age != 25):
+#     print("you are not 25")
+
+# if (age > 25):
+#     print("you are over 25")
+
+# if age >= 21:
+#     print("You get free beer")
+# else:
+#     print("Sorry no beer for you")
+
+# if age >= 21:
+#   print("You get free beer")
+# elif age < 18:
+#   print("What are you even doing here?")
+# else:
+#   print("Sorry no beer for you")
+
+
+# greeting = "Hello {}, it is very nice to meet you and your friend {}!"
+# name_of_user = input("What is your name? ")
+# length_of_name = len(name_of_user)
+# if length_of_name > 6:
+#     name_of_friend = input("What is your friend's name? ")
+#     print(greeting.format(name_of_user, name_of_friend))
+# elif length_of_name > 7:
+#     name_of_friend = input("What is your friend's name? ")
+#     print(greeting.format(name_of_user, name_of_friend))
+# else:
+#     print("OK, I'll ask again some other time.")
+
+
+# position matters where you put the count value
+# count = 0
+# while(count < 10):
+    
+#     count += 1
+#     print("The count is ", count)
+
+
+answer = ''
+while answer != 'when':
+  answer = input('Say when: ')
+  answer = answer.lower()
+print("Cheese")
+
+# while True:
+#   answer = input('Say when: ')
+#   if answer.lower() == 'when':
+#     break
+# print("Cheese")
+
