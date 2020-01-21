b1c8c9bf7c981bfd84dea407fa4d7a9e596c08fa
diego rivera <diegora1377@gmail.com>
2020-01-21 10:10:45 -0600

    week1_hw_friday

---
+++ medium_friday.py
@@ -0,0 +1,42 @@
+#1
+# enter_word = input("Please enter a word: ")
+# def word_freq(word):
+#     my_dict = {}
+#     for letter in word:
+#         keys = my_dict.keys()
+#         if letter in keys:
+#             my_dict[letter] += 1
+#         else:
+#             my_dict[letter] = 1
+#     return my_dict
+# print(word_freq(enter_word))
+
+
+
+#2
+# enter_sentence = input("Please enter a sentence: ")
+# def sent_freq(sentence):
+#     count = dict()
+#     words = sentence.split()
+#     for word in words:
+#         if word in count:
+#             count[word] += 1
+#         else:
+#             count[word] = 1
+#     return count
+# print(sent_freq(enter_sentence))
+
+
+#3
+# enter_word = input("Please enter a word: ")
+# def word_freq(word):
+#     my_dict = {}
+#     for letter in word:
+#         keys = my_dict.keys()
+#         if letter in keys:
+#             my_dict[letter] += 1
+#         else:
+#             my_dict[letter] = 1
+#     return my_dict
+# sort_word = sorted(word_freq(enter_word))
+# print(sort_word[:3])
